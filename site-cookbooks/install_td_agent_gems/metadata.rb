3eea44105db9740a0dfd59e358e0965acc8516c3
onigra <3280467rec@gmail.com>
2013-06-12 12:48:14 +0900

    td-agentのgemをインストールする

---
+++ site-cookbooks/install_td_agent_gems/metadata.rb
@@ -0,0 +1,7 @@
+name             'install_td_agent_gems'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures install_td_agent_gems'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ site-cookbooks/install_td_agent_gems/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: install_td_agent_gems
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+gems = [
+  'fluent-plugin-mysqlslowquery',
+  'fluent-plugin-redis'
+]
+
+gems.each do |g|
+  gem_package "#{g}" do
+    gem_binary "/usr/lib64/fluent/ruby/bin/fluent-gem"
+    action :install
+  end
+end
+
