b9d051f67502d5656d99f914ca634d1971d04f83
onigra <3280467rec@gmail.com>
2013-06-13 16:47:12 +0900

    rubyのインストール時にgemも入れるように修正

---
+++ site-cookbooks/ruby-rbenv/recipes/gem_install.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: ruby-rbenv
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+install_gem_list = [
+  "bundler",
+  "pry"
+]
+
+install_gem_list.each do |g|
+  gem_package "#{g}" do
+    gem_binary "/usr/local/rbenv/shims/gem"
+    options "--no-rdoc --no-ri"
+    action :install
+  end
+end
+
+execute "rbenv_rehash" do
+  command "source /etc/profile.d/rbenv.sh; rbenv rehash"
+  action :run
+end
