8af7d85a232f8cc805c0e8ec81d10a8113e1f96a
onigra <3280467rec@gmail.com>
2014-01-22 16:30:46 +0900

    serverspecは別管理にした

--- Gemfile
+++ Gemfile
@@ -4,6 +4,4 @@ source "https://rubygems.org"
 gem 'chef'
 gem 'berkshelf'
 gem 'knife-solo', '0.3.0.pre3'
-gem 'serverspec'
-gem 'rake'
 
--- spec/git-ruby/git_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command('/usr/local/bin/git --version') do
-  its(:stdout) { should match 'git version 1.8.5.3'}
-end
